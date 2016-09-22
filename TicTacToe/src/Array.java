cc0d457ffc4c8734a5a13f7d2f19ae26cae7d927
mcguire <mcguire@mullenhigh.com>
2016-09-22 11:02:12 -0600

    add isWonGame() method

--- TicTacToe/src/Array.java
+++ TicTacToe/src/Array.java
@@ -88,6 +88,26 @@ public static void computerMove()
 			computerMove();
 		}
 	
+//	Here is a method I use to determine if the game is
+//	won.  Feel free to adapt this to your program.
+//	
+//	public static boolean isWonGame()
+//		{
+//		if ((board[0][0].equals(board[0][1]) && board[0][1].equals(board[0][2]) && !board[0][1].equals(" ")) ||
+//			(board[1][0].equals(board[1][1]) && board[1][1].equals(board[1][2]) && !board[1][1].equals(" ")) ||
+//			(board[2][0].equals(board[2][1]) && board[2][1].equals(board[2][2]) && !board[2][1].equals(" ")) ||
+//			(board[0][0].equals(board[1][0]) && board[1][0].equals(board[2][0]) && !board[1][0].equals(" ")) ||				
+//			(board[0][1].equals(board[1][1]) && board[1][1].equals(board[2][1]) && !board[1][1].equals(" ")) ||
+//			(board[0][2].equals(board[1][2]) && board[1][2].equals(board[2][2]) && !board[1][2].equals(" ")) ||
+//			(board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[1][1].equals(" ")) ||
+//			(board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[1][1].equals(" ")))
+//			{
+//			System.out.println("The game is over!");
+//			return true;
+//			}
+//		return false;
+//		}
+	
 	
 }
 }
